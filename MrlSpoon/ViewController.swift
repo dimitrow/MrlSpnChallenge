cc9dc54a530076d5afa7addd4ea92cda8872921e
gene.dimitrow <e.dimitrow@gmail.com>
2021-08-10 22:43:20 +0300

    -project initial / got rid of storyboard

---
+++ MrlSpoon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MrlSpoon
+//
+//  Created by Gene Dimitrow on 10.08.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
